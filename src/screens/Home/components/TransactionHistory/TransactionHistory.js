0b04bcce9bfee186e5d6cab5884ac9121a329411
Rex Cornes <rexcornes@yahoo.com>
2020-12-12 15:03:35 +0800

    Dummy transaction, installedd moment

--- src/screens/Home/components/TransactionHistory/TransactionHistory.js
+++ src/screens/Home/components/TransactionHistory/TransactionHistory.js
@@ -1,9 +1,22 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native';
-import { DataTable, Card, Title, Searchbar } from 'react-native-paper';
+import { DataTable, Card, Title } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import dummyTransactions from './components/dummyTransaction'
+import { formatDate } from './../../../../shared/utils'
+
 const TransactionHistory = (props) => {
+  const TransactionHistoryComponent = dummyTransactions.map(item => {
+    return(
+      <DataTable.Row  onPress={() => props.navigation.navigate('Transaction Details')}>
+        <DataTable.Cell>{item.body.deliveryRecieveDate} </DataTable.Cell>
+        <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
+        <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
+        <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
+      </DataTable.Row>
+    )
+  })
+
   return(
     <Card style={{margin: 10 }}>
       <Card.Content>
@@ -13,72 +26,13 @@ const TransactionHistory = (props) => {
               <Title style={{fontFamily: 'Lato-Bold', fontSize: 16}}>TRANSACTION HISTORY</Title>
             </DataTable.Title>
           </DataTable.Header>
-          <DataTable.Header >
+          <DataTable.Header>
             <DataTable.Title>Date</DataTable.Title>
-            <DataTable.Title style={{marginLeft: 10}}>Transaction</DataTable.Title>
-            <DataTable.Title style={{marginLeft: 40}}>Buyer</DataTable.Title>
-            <DataTable.Cell ></DataTable.Cell>
+            <DataTable.Title>Description</DataTable.Title>
+            <DataTable.Title>Status</DataTable.Title>
+            <DataTable.Title></DataTable.Title>
           </DataTable.Header>
-          <DataTable.Row  onPress={() => props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row onPress={()=> props.navigation.navigate('Transaction Details')}>
-            <DataTable.Cell>12/02/2020 </DataTable.Cell>
-            <DataTable.Cell numeric>Bought wholesale malunggay</DataTable.Cell>
-            <DataTable.Cell numeric>Juan Cruz</DataTable.Cell>
-            <DataTable.Cell numeric><MaterialCommunityIcons name="chevron-right" color="#6200EE" size={30} /></DataTable.Cell>
-          </DataTable.Row>
+         
           <DataTable.Pagination
             page={1}
             numberOfPages={3}
---
+++ src/screens/Home/components/TransactionHistory/components/dummyTransaction.js
@@ -0,0 +1,73 @@
+const dummyTransactions = [
+  {
+    "success": true,
+    "body": {
+      "transactionId": 1,
+      "wholesalerId": 1,
+      "goodsId": 1,
+      "status": 1,
+      "deliverySendDate": "2020-12-10T18:32:17.000Z",
+      "deliveryRecieveDate": "2020-12-11T18:32:17.000Z",
+      "deliveryDesc": "Item well packed with breathable plastic",
+      "createdAt": "2020-12-10T18:32:17.000Z",
+      "updatedAt": "2020-12-11T18:32:17.000Z"
+    },
+  },
+  {
+    "success": true,
+    "body": {
+      "transactionId": 1,
+      "wholesalerId": 1,
+      "goodsId": 1,
+      "status": 1,
+      "deliverySendDate": "2020-12-10T18:32:17.000Z",
+      "deliveryRecieveDate": "2020-12-11T18:32:17.000Z",
+      "deliveryDesc": "Item well packed with breathable plastic",
+      "createdAt": "2020-12-10T18:32:17.000Z",
+      "updatedAt": "2020-12-11T18:32:17.000Z"
+    }
+  },
+  {
+    "success": true,
+    "body": {
+      "transactionId": 1,
+      "wholesalerId": 1,
+      "goodsId": 1,
+      "status": 1,
+      "deliverySendDate": "2020-12-10T18:32:17.000Z",
+      "deliveryRecieveDate": "2020-12-11T18:32:17.000Z",
+      "deliveryDesc": "Item well packed with breathable plastic",
+      "createdAt": "2020-12-10T18:32:17.000Z",
+      "updatedAt": "2020-12-11T18:32:17.000Z"
+    }
+  },
+  {
+    "success": true,
+    "body": {
+      "transactionId": 1,
+      "wholesalerId": 1,
+      "goodsId": 1,
+      "status": 1,
+      "deliverySendDate": "2020-12-10T18:32:17.000Z",
+      "deliveryRecieveDate": "2020-12-11T18:32:17.000Z",
+      "deliveryDesc": "Item well packed with breathable plastic",
+      "createdAt": "2020-12-10T18:32:17.000Z",
+      "updatedAt": "2020-12-11T18:32:17.000Z"
+    }
+  },{
+    "success": true,
+    "body": {
+      "transactionId": 1,
+      "wholesalerId": 1,
+      "goodsId": 1,
+      "status": 1,
+      "deliverySendDate": "2020-12-10T18:32:17.000Z",
+      "deliveryRecieveDate": "2020-12-11T18:32:17.000Z",
+      "deliveryDesc": "Item well packed with breathable plastic",
+      "createdAt": "2020-12-10T18:32:17.000Z",
+      "updatedAt": "2020-12-11T18:32:17.000Z"
+    }
+  }
+]
+
+export default  dummyTransactions
