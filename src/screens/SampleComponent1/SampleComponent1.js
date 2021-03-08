f6f4e30ee4369cbc5ad44aeed332b39fd1489615
Bandaggao <ban_daggao@questronix.com.ph>
2021-03-08 17:49:42 +0800

    [Feat]: Geo location

--- src/screens/SampleComponent1/SampleComponent1.js
+++ src/screens/SampleComponent1/SampleComponent1.js
@@ -1,20 +1,68 @@
-import React from 'react'
-import {StyleSheet,View,Text} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const SampleComponent1 = () =>{
-  return(
-    <View style={styles.container}>
-      <Text>Blank Screen</Text>
-    </View>
-  )
+
+export default function Sample() {
+  const [mainMarker, setMainMarker] = useState()
+  const [mainMap, setMainMap] = useState()
+
+  const findCoordinates =()=> {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const longitude = JSON.stringify(position.coords.longitude);
+        const latitude = JSON.stringify(position.coords.latitude);
+
+        const marker = (
+          <MapView               
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation
+          initialRegion={{
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421}}>
+
+          <Marker coordinate = {{latitude: parseFloat(latitude),longitude: parseFloat(longitude)}}
+          pinColor = {"orange"} // any color
+          title={"title"}
+          description={"description"}/>
+          </MapView>
+        )
+
+        setMainMarker(marker)
+  
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+    );
+  };  
+
+  useEffect(()=>{
+    findCoordinates()
+  },[])
+  
+    return (
+      <>
+        {mainMarker}
+      </>
+
+    );
+  
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
   }
 })
-
-export default SampleComponent1
--- src/screens/SampleComponent2/SampleComponent2.js
+++ src/screens/SampleComponent2/SampleComponent2.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import {StyleSheet,View,Text} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const SampleComponent2 = () =>{
-  return(
-    <View style={styles.container}>
-      <Text>Blank Screen</Text>
-    </View>
-  )
+
+
+export default function Sample2() {
+    return ( 
+      <>
+      <MapView
+      
+               style={{ flex: 1 }}
+               provider={PROVIDER_GOOGLE}
+               showsUserLocation
+               initialRegion={{
+               latitude: 37.78825,
+               longitude: -122.4324,
+               latitudeDelta: 0.0922,
+               longitudeDelta: 0.0421}}
+      
+            >
+        <Marker coordinate = {{latitude: 37.78825,longitude: -122.4324}}
+         pinColor = {"orange"} // any color
+         title={"title"}
+         description={"description"}/>
+
+            </MapView>
+
+</>
+    );
+  
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
   }
 })
-
-export default SampleComponent2
